ee2da34a6362069df193704c166a137ab828814b
Eashan <eashankadam@gmail.com>
2020-06-16 18:53:00 +0530

    Init

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: "Enu's Design Project",
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Welcome to my Design Project</h1>
+    <p>I am gonna try to learn as much as possible
+
+    </p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
