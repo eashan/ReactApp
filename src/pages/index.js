62f1647d0b0555d0c551d677747281f9e9c8ab46
Eashan <eashankadam@gmail.com>
2020-06-17 13:15:35 +0530

    Basic CSS Lec

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,14 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Welcome to my Design Project</h1>
-    <p>I am gonna try to learn as much as possible
-
-    </p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Welcome to my Design Project</h1>
+        <p>I am gonna try to learn as much as possible</p> 
+        <Link to="/page-2/">Watch the Video</Link>
+      </div>
+    </div>
+    
   </div>
 )
 
