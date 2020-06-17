8cf424dbef3cca7ff0dbae095a4164edcd1c9e2b
Eashan <eashankadam@gmail.com>
2020-06-17 21:09:33 +0530

    Adaptive Layouts with CSS grid

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <div>
     <div className="Hero">
       <div className="HeroGroup">
-        <h1>Welcome to my Design Project</h1>
+        <h1>Welcome to <br/> my Design Project</h1>
         <p>I am gonna try to learn as much as possible</p> 
         <Link to="/page-2/">Watch the Video</Link>
       </div>
